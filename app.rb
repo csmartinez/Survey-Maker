a8daa8e537765ac4b5df6fba6ea613db3efbdba5
Carli Martinez & Frank Meza <student@epicodus.com>
2015-01-28 15:12:19 -0800

    fix small CSS bug, fix major Sinatra bug, remove ID element from url addresses for learning purposes.

--- app.rb
+++ app.rb
@@ -12,19 +12,33 @@ post('/surveys') do
   name = params.fetch('name')
   @name = Survey.create({:name => name})
   redirect('/')
-  erb(:index)
-end
-
-post('/surveys/:id') do
-  ask = params.fetch("ask")
-  @survey = Survey.find(params.fetch("id").to_i())
-  @ask = Question.create({:ask => ask, :survey_id => (params.fetch("id"))})
-  redirect('/surveys')
-  erb(:surveys)
 end
 
 get('/surveys/:id') do
   @survey = Survey.find(params.fetch("id").to_i())
   @questions = Question.all()
   erb(:surveys)
+end
+
+post('/add_question') do
+  ask = params.fetch("ask")
+  @survey = Survey.find(params.fetch("survey_id").to_i())
+  Question.create({:ask => ask, :survey_id => (params.fetch("survey_id"))})
+  erb(:surveys)
+end
+
+patch('survey/edit_or_delete') do
+  name = params.fetch('name')
+  @survey = Survey.find(params.fetch("survey_id").to_i())
+  @survey.update({:name => name})
+  @surveys = Survey.all()
+  redirect('/')
+  erb(:edit_survey)
+end
+
+delete('survey/edit_or_delete') do
+  @survey = Survey.find(params.fetch("survey_id").to_i())
+  @survey.destroy()
+  redirect('/')
+  erb(:edit_survey)
 end
